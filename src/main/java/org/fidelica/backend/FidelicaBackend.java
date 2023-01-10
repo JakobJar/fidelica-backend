b01ad20556c5a12580867aee495e71b6442547fb
Jakob <jakob.stiel@gmail.com>
2023-01-10 11:05:32 +0100

    Add logger

--- src/main/java/org/fidelica/backend/FidelicaBackend.java
+++ src/main/java/org/fidelica/backend/FidelicaBackend.java
@@ -1,8 +1,16 @@
 package org.fidelica.backend;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class FidelicaBackend {
 
     public static void main(String[] args) {
-
+        log.info("   __  _      _        _  _             ");
+        log.info("  / _|(_)    | |      | |(_)            ");
+        log.info(" | |_  _   __| |  ___ | | _   ___  __ _ ");
+        log.info(" |  _|| | / _` | / _ \\| || | / __|/ _` |");
+        log.info(" | |  | || (_| ||  __/| || || (__| (_| |");
+        log.info(" |_|  |_| \\__,_| \\___||_||_| \\___|\\__,_|");
     }
 }
