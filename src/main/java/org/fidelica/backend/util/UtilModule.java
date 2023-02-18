8c3407ca1e68f1c8276807f5066ed70eaaddf127
Jakob <jakob.stiel@gmail.com>
2023-02-18 15:34:46 +0100

    Remove RecaptchaProvider

--- src/main/java/org/fidelica/backend/util/UtilModule.java
+++ src/main/java/org/fidelica/backend/util/UtilModule.java
@@ -1,12 +1,21 @@
 package org.fidelica.backend.util;
 
+import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+
+import java.net.http.HttpClient;
 
 public class UtilModule extends AbstractModule {
 
-    @Override
-    protected void configure() {
-        bind(GoogleRecaptcha.class).toProvider(RecaptchaProvider.class).in(Scopes.SINGLETON);
+    @Provides
+    @Singleton
+    private GoogleRecaptcha getRecaptcha(HttpClient httpClient, Gson gson, @Named("RECAPTCHA KEY") String secretKey) {
+        if (secretKey != null && !secretKey.isBlank()) {
+            return new GoogleRecaptchaV3(httpClient, gson, secretKey);
+        }
+        return new DummyGoogleRecaptcha();
     }
 }
