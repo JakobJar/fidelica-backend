dfce97574a8fc55cd6efba5f5ad81f537a208db7
Jakob <jakob.stiel@gmail.com>
2023-03-05 19:49:14 +0100

    Bind group repository

--- src/main/java/org/fidelica/backend/repository/RepositoryModule.java
+++ src/main/java/org/fidelica/backend/repository/RepositoryModule.java
@@ -6,6 +6,8 @@ import org.fidelica.backend.repository.article.FactCheckRepository;
 import org.fidelica.backend.repository.article.StandardFactCheckRepository;
 import org.fidelica.backend.repository.post.twitter.StandardTwitterRepository;
 import org.fidelica.backend.repository.post.twitter.TwitterRepository;
+import org.fidelica.backend.repository.user.GroupRepository;
+import org.fidelica.backend.repository.user.StandardGroupRepository;
 import org.fidelica.backend.repository.user.StandardUserRepository;
 import org.fidelica.backend.repository.user.UserRepository;
 
@@ -14,6 +16,7 @@ public class RepositoryModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(UserRepository.class).to(StandardUserRepository.class).in(Scopes.SINGLETON);
+        bind(GroupRepository.class).to(StandardGroupRepository.class).in(Scopes.SINGLETON);
         bind(FactCheckRepository.class).to(StandardFactCheckRepository.class).in(Scopes.SINGLETON);
 
         bind(TwitterRepository.class).to(StandardTwitterRepository.class).in(Scopes.SINGLETON);
